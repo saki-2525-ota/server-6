99bbb155e1258e3d88a940c0ea88c929fc7e9eab
saki-2525-ota <206808265+saki-2525-ota@users.noreply.github.com>
2025-10-31 11:33:30 +0900

    リクエスト処理追加

--- server.js
+++ server.js
@@ -9,4 +9,19 @@ app.get('/api', async (c) => {
   return c.json({ message: 'GET' });
 });
 
+// POSTリクエストに対する処理
+app.get('/api', async (c) => {
+  return c.json({ message: 'POST' });
+});
+
+// PUTリクエストに対する処理
+app.get('/api', async (c) => {
+  return c.json({ message: 'PUT' });
+});
+
+// DELETEリクエストに対する処理
+app.get('/api', async (c) => {
+  return c.json({ message: 'DELETE' });
+});
+
 Deno.serve(app.fetch);
